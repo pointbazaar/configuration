21c4b31e80720f5bd840953aa6adf4d987558bf7
pointbazaar <pointbazaar@protonmail.com>
2018-09-30 17:28:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 !/bin/bash
 
+#run with sudo
 #some things every dev in our organization needs
 
 apt install -y gedit
@@ -7,3 +8,5 @@ apt install -y gcc
 apt install -y vim
 apt install -y vlc
 apt install -y baobap
+apt install -y default-jdk
+apt install -y nodejs
