5279e5dbeeaa85d8f4bfc7676e221e0ca0aba9ba
pointbazaar <pointbazaar@protonmail.com>
2018-11-30 13:48:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,18 @@
 #run with sudo
 #some things every dev in our organization needs
 
+#editors
 apt install -y gedit
-apt install -y gcc
 apt install -y vim
+
+apt install -y gcc
 apt install -y vlc
 apt install -y baobap
 apt install -y default-jdk
 apt install -y nodejs
+
+#build tools
+apt install -y maven
 
 #for inspecting and developing REST APIs
 snap install postman 
