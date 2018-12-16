abefe98d726a7314c23b6672051b95b250941c58
pointbazaar <pointbazaar@protonmail.com>
2018-12-16 21:00:45 +0100

    add atom install

--- setup.sh
+++ setup.sh
@@ -21,4 +21,12 @@ apt install -y maven
 apt install -y inkscape
 
 #for inspecting and developing REST APIs
-snap install postman 
+snap install postman
+
+#install atom
+curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update 
+sudo apt-get install atom
+
+
