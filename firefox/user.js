3a7acf5168b5ab9708e5138bdf9bfefc906a89ff
pointbaaar <alex23667@gmail.com>
2019-02-19 18:03:59 +0100

    make user.js for firefox

---
+++ firefox/user.js
@@ -0,0 +1,13 @@
+// Mozilla User Preferences
+
+// To change a preference value, you can either:
+// - modify it via the UI (e.g. via about:config in the browser); or
+// - set it within a user.js file in your profile.
+
+user_pref("browser.newtabpage.enhanced", false);
+
+user_pref("layout.css.font-display.enabled", false);
+user_pref("layout.css.font-loading-api.enabled", false);
+user_pref("layout.css.font-variations.enabled", false);
+
+
