8909b7cfed6e9c9fa713b721e3b4b8971fe60553
pointbaaar <alex23667@gmail.com>
2019-02-19 18:13:13 +0100

    move around preferences

--- firefox/user.js
+++ firefox/user.js
@@ -10,4 +10,28 @@ user_pref("layout.css.font-display.enabled", false);
 user_pref("layout.css.font-loading-api.enabled", false);
 user_pref("layout.css.font-variations.enabled", false);
 
+//user_pref("",false);
+//user_pref("layout.css.",false);
+//user_pref("layout.css..enabled",false);
+user_pref("layout.css.font-variations.enabled",false);
+user_pref("layout.css.individual-transform.enabled",false);
+user_pref("layout.css.initial-letter.enabled",false);
+user_pref("layout.frame_rate",30);
 
+user_pref("layout.css.prefixes.font-features",false);
+user_pref("layout.css.prefixes.transforms",false);
+user_pref("layout.css.prefixes.transitions",false);
+user_pref("layout.css.prefixes.animations",false);
+
+user_pref("layout.css.scrollbar-color.enabled",false);
+user_pref("layout.css.scrollbar-width.enabled",false);
+user_pref("layout.css.text-justify.enabled",false);
+user_pref("layout.css.background-blend-mode-enabled",false);
+
+user_pref("layout.css.contain.enabled",false);
+user_pref("layout.css.motion-path.enabled",false);
+
+user_pref("nglayout.enable_drag_images",false);
+
+user_pref("zoom.minPercent",100);
+user_pref("zoom.maxPercent",100);
