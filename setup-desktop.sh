d24415620543d7acb676bc2a4c9975db3cee72e4
pointbazaar <alex23667@gmail.com>
2019-12-02 18:00:40 +0100

    minor changes

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-bash setup-common.sh
-#some things every dev in our organization needs
 
 #editors
 apt install -y gedit geany
 
-#IDE's
-#intellij is too heavyweight and uses too much resources
-#it cannot be used reliably without a high-end computer
-#sudo snap install intellij-idea-community --classic
-
-
-
 #graphic tools
 #vector graphics - inkscape
 apt install -y inkscape
-
-#for inspecting and developing REST APIs, dont use postman bc its commercial product
-
-
-
 
 #pdf, images, videos
 apt install -y feh vlc xpdf
