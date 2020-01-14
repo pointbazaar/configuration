e29dd2665e6c255f41bfcb31d4696b8a1658aa47
pointbazaar <alex23667@gmail.com>
2020-01-14 18:55:05 +0100

    minor changes

--- limit_cpufreq.sh
+++ limit_cpufreq.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+
+#this script limits the maximum cpu frequency.
+#it is taken from an answer by Doug Smythies on stackoverflow
+#it is intended to give longer battery life to your laptop.
+
+#certain workloads will take longer, but lower frequency means lower power consumption
+
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq; do echo "2900000" > $file; done
+for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq; do echo "1400000" > $file; done
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
