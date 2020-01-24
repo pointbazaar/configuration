5ccb46b7a837d1d9985ec3a1b2de8607217e34e0
pointbazaar <alex23667@gmail.com>
2020-01-24 05:52:24 +0100

    upgrade to latest version of jooq

--- download_jooq_jars.sh
+++ download_jooq_jars.sh
@@ -13,6 +13,9 @@ wget https://repo1.maven.org/maven2/org/jooq/jooq/3.12.3/jooq-3.12.3.jar
 wget https://repo1.maven.org/maven2/org/jooq/jooq-codegen/3.12.3/jooq-codegen-3.12.3.jar
 wget https://repo1.maven.org/maven2/org/jooq/jooq-meta/3.12.3/jooq-meta-3.12.3.jar
 
-wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.18/mysql-connector-java-8.0.18.jar
+wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.18/mysql-connector-java-5.1.18.jar
 
 wget https://repo1.maven.org/maven2/javax/activation/activation/1.1.1/activation-1.1.1.jar
+
+wget https://search.maven.org/remotecontent?filepath=org/reactivestreams/reactive-streams/1.0.3/reactive-streams-1.0.3.jar
+mv remotecontent\?filepath\=org%2Freactivestreams%2Freactive-streams%2F1.0.3%2Freactive-streams-1.0.3.jar reactive-streams-1.0.3.jar
