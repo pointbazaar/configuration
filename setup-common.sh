b50fcb2d174165e484427d15fae910577bd1caa2
pointbazaar <alex23667@gmail.com>
2020-03-14 03:10:46 +0100

    Update setup-common.sh

--- setup-common.sh
+++ setup-common.sh
@@ -6,15 +6,6 @@ apt-get update
 #version control: git
 apt install -y git
 
-
-#display comments
-git clone https://github.com/pointbazaar/comcat.git
-cd comcat
-./build.sh
-cd ..
-rm -rf comcat
-
-
 #for drawing graphs and such
 apt install -y graphviz 
 
@@ -22,7 +13,6 @@ apt install -y graphviz
 git config credential.helper 'cache --timeout=3000000'
 #set global gitignore, we already have downloaded this repo, it should be in home directory
 git config --global core.excludesfile '~/configuration/.gitignore'
-
 
 
 #haskell is big and takes long to install especially with hackage and all the packages
@@ -52,7 +42,6 @@ apt install -y time
 apt install -y screenfetch
 #build tools
 apt install -y maven
-
 
 
 apt install -y net-tools
