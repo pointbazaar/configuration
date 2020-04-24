90c7df224a18d99517e676ac47095aba70632052
pointbazaar <alex23667@gmail.com>
2020-04-24 23:15:15 +0200

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -4,5 +4,16 @@ bash setup-common.sh
 
 #https://www.digitalocean.com/community/tutorials/how-to-configure-nginx-as-a-reverse-proxy-for-apache
 #server stuff
-apt install -y nginx
+
+
+#apt install -y nginx
 #apt install -y apache2
+
+apt install ufw
+
+#enable ufw
+#ufw allow 8080
+ufw enable
+ufw status
+
+
