058bcfbe54656e2ab2043fddeecee2ddb67f354b
pointbazaar <alex23667@gmail.com>
2020-06-26 20:19:26 +0200

    update

--- setup-desktop.sh
+++ setup-desktop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 bash setup-common.sh
 
-#editors
-apt install -y gedit geany
+#editors:
+#texstudio is for editing latex
+apt install -y gedit geany texstudio
 
 #graphic tools
 #vector graphics - inkscape
@@ -15,6 +15,5 @@ apt install -y feh vlc xpdf
 #IDE for C/C++ development
 apt install -y codeblocks
 
-#convenient ssh client that remembers 
-#hosts and credentials
+#convenient ssh client that remembers hosts and credentials
 snap install termius-app
