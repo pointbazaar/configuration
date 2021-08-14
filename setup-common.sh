cf370ff675dba6439c0a9c5919bc04e86220d11f
pointbazaar <alex23667@gmail.com>
2021-08-14 03:12:27 +0200

    update

--- setup-common.sh
+++ setup-common.sh
@@ -7,15 +7,6 @@ apt install -y git
 #for drawing graphs and such
 apt install -y graphviz 
 
-#configure git to cache credentials
-git config credential.helper 'cache --timeout=3000000'
-#set global gitignore, we already have downloaded this repo, it should be in home directory
-git config --global core.excludesfile '~/configuration/.gitignore'
-
-#haskell
-#apt install -y ghc haskell-stack
-#stack upgrade
-
 #C toolchain, nasm,  etc
 apt install -y gcc make nasm  
 
@@ -27,10 +18,6 @@ apt install -y screenfetch
 #build tools
 apt install -y maven
 
-apt install -y net-tools
-
-#don't forget letsencrypt at certbot
-
 #see a tree view of filesystem
 snap install -y tree
 
@@ -39,7 +26,7 @@ ln -s .vimrc ~/
 
 #Java OpenJdk
 #alternative: default-jdk
-sudo apt install -y openjdk-14-jdk  
+sudo apt install -y openjdk-16-jdk  
 
 #MYSQL Client
 sudo apt install -y mysql-client-core-8.0     # version 8.0.20-0ubuntu0.20.04.1
--- setup-desktop.sh
+++ setup-desktop.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 bash setup-common.sh
 
-#graphical disk usage analyzer
-apt install -y baobap 
-
 #editors:
-#texstudio is for editing latex
 #geany is for my C Projects
-apt install -y gedit geany texstudio
+apt install -y gedit geany 
 
 #pdf, images, videos, svg editing
 apt install -y feh vlc xpdf inkscape
@@ -18,9 +14,6 @@ snap install termius-app
 #install intellij
 snap install --classic intellij-idea-community
 
-#install android studio
-#snap install --classic android-studio
-
 #-----------------------------------------
 #install mysql-workbench
 #HINT: you need to enable some permissions
@@ -30,7 +23,3 @@ snap install --classic intellij-idea-community
 #To copy connections to other computers:
 #Tools->Configuration->Backup Connections
 snap install --candidate mysql-workbench-community
-
-#install youtube-dl to download videos from youtube
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
