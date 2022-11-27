4b23f2f873b8886e936a2caddb5f40f7e9f88029
Alexander Hansen <alex23667@gmail.com>
2022-11-27 02:44:40 +0100

    update polybar config

--- preonic-keyboard/keymap.c
+++ preonic-keyboard/keymap.c
@@ -206,24 +206,6 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 
 
 void matrix_scan_user(void) {
-#ifdef AUDIO_ENABLE
-    if (muse_mode) {
-        if (muse_counter == 0) {
-            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-            if (muse_note != last_muse_note) {
-                stop_note(compute_freq_for_midi_note(last_muse_note));
-                play_note(compute_freq_for_midi_note(muse_note), 0xF);
-                last_muse_note = muse_note;
-            }
-        }
-        muse_counter = (muse_counter + 1) % muse_tempo;
-    } else {
-        if (muse_counter) {
-            stop_all_notes();
-            muse_counter = 0;
-        }
-    }
-#endif
 }
 
 bool music_mask_user(uint16_t keycode) {
