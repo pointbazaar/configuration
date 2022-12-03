5779255e7351c29cfc83a385f6a39eff4969702c
Alexander Hansen <alex23667@gmail.com>
2022-12-03 15:15:54 +0100

    update

---
+++ setup-wifi.sh
@@ -0,0 +1,9 @@
+
+# separate script to setup wifi (for laptops, ...)
+apt install -y network-manager
+
+# might be required
+#apt install -y firmware-iwlwifi
+
+#nmcli dev wifi list
+#nmcli dev wifi connect $network-ssid password $password
