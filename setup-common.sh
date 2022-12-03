a3f00d953865855f2327cbb42b9feeb959b3e304
Alexander Hansen <alex23667@gmail.com>
2022-12-03 01:24:58 +0100

    update

--- setup-common.sh
+++ setup-common.sh
@@ -3,7 +3,12 @@
 
 apt-get update
 
+
+# setup git
 apt install -y git
+git config --global user.email "alex23667@gmail.com"
+git config --global user.name "Alexander Hansen"
+
 
 # -- ssh utilities --
 # enables ssh with password on the commandline
--- setup-gitconfig.sh
+++
@@ -1,3 +0,0 @@
-
-git config --global user.email "alex23667@gmail.com"
-git config --global user.name "Alexander Hansen"
