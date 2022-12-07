e832f7df6289de99a7f3c4fb9cfbf456e807dadd
Alexander Hansen <alexander.hansen@9elements.com>
2022-12-07 13:07:55 +0100

    update email

--- setup-common.sh
+++ setup-common.sh
@@ -6,7 +6,7 @@ apt-get update
 
 # setup git
 apt install -y git
-git config --global user.email "alex23667@gmail.com"
+git config --global user.email "alexander.hansen@9elements.com"
 git config --global user.name "Alexander Hansen"
 
 
